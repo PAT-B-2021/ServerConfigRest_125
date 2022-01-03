0c7e514c377805519f1c4655c450b858bd6b010d
ridwannurfaizin12 <ridwannurfaizin12>
2022-01-03 23:30:26 +0700

    Add project files.

---
+++ SeverConfigRest_125_Ridwan/Program.cs
@@ -0,0 +1,33 @@
+﻿using ServiceRest_125_Ridwan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeverConfigRest_125_Ridwan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server ready ..");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
